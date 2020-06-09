7454ddb539038086fb6ffd5b5aa6da86029f51bd
Noor khan <62239655+abdulnoorkhanwaleed@users.noreply.github.com>
2020-06-09 18:24:21 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>my name is noor khan waleed</h1>
+      <h1>my name is <strong>Noor Khan</strong>noor khan waleed</h1>
+    
     </div>
   );
 }
