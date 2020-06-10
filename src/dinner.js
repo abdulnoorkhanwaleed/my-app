bcf1e7cac52fd46787ac3b121183c21ef8038981
Noor khan <62239655+abdulnoorkhanwaleed@users.noreply.github.com>
2020-06-10 07:11:10 +0500

    Update dinner.js

--- src/dinner.js
+++ src/dinner.js
@@ -3,7 +3,7 @@ function Dinner(props){
     return(
         <div>
 
-            <h1>My full name is =>>{props.myName} </h1>
+            <h1>My full name is {props.myName} </h1>
             <p>And i am studing in {props.uniName}</p>
         </div>
     )
