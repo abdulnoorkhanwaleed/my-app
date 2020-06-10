41bb937cb65c6fe0873cf33ed0ea53614112a3fb
Noor khan <62239655+abdulnoorkhanwaleed@users.noreply.github.com>
2020-06-10 07:43:58 +0500

    Update dinner.js

--- src/dinner.js
+++ src/dinner.js
@@ -4,6 +4,7 @@ function Dinner(props){
         <div>
 
             <h1>My full name is {props.myName} </h1>
+            <hr/>
             <p>And i am studing in {props.uniName}</p>
         </div>
     )
